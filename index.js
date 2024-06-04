98d24b39ddfdc296f4086ee05e096c809e199e90
Nicholas <bestnicholas05@gmail.com>
2024-06-03 21:20:18 -0400

    Completed Fetch Lab 1

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  //fetch('https://anapioficeandfire.com/api/books')
+    //.then(response => response.json()) // Convert the response to JSON
+    //.then(data => renderBooks(data)); // Pass the JSON data to renderBooks function
+    return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Convert the response to JSON
+    })
+    .then(data => renderBooks(data)) // Pass the JSON data to renderBooks function
+    .catch(error => console.error('Error fetching books:', error)); // Handle any errors
 }
 
 function renderBooks(books) {
